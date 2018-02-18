044c36327036185a7229869e0ee04fef5b43ec58
Lenin Hasda <leninhasda@gmail.com>
2018-02-19 00:25:09 +0600

    done day27 29

--- hacker-rank/30-days-of-code/day-27-testing.py
+++ hacker-rank/30-days-of-code/day-27-testing.py
@@ -1,13 +1,11 @@
-print('6')
-print('5 2')
-print('0 1 2 1 4')
-print('5 2')
-print('0 -1 2 1 4')
-print('5 2')
-print('-1 -1 2 1 4')
-print('5 2')
-print('-2 -1 2 1 4')
-print('5 2')
-print('-3 -1 2 1 4')
-print('5 2')
-print('-4 -1 2 1 4')
+def showTest(n, k, a):
+    print str(n) + " " + str(k)
+    print " ".join(map(str, a))
+
+testCases = 5
+print testCases
+showTest(4, 3, [-1, 0, 4, 2])
+showTest(5, 2, [0, -1, 2, 1, 4])
+showTest(7, 6, [2, 0, -1, 1, 1, 1, 1])
+showTest(3, 1, [-1, 0, 4])
+showTest(6, 4, [0, -1, 1, 4, 5, 6])
