8b0a057602aea34be890e382569b11ebab83d6ef
Lenin Hasda <lenin.hasda@codersrank.io>
2019-01-12 06:09:04 +0600

    palindrome number

---
+++ leetcode/9-palindrome-number.go
@@ -0,0 +1,25 @@
+func isPalindrome(x int) bool {
+  
+  if x < 0 {
+    return false
+  }
+  
+  var i int
+  var xs []int
+  for {
+    i, x = x % 10, x / 10 
+    xs = append(xs, i)
+    if x == 0 {
+      break
+    }
+  }
+  
+  ln := len(xs)
+  for i=0; i < ln/2; i++ {
+    if xs[i] != xs[ln-i-1] {
+      return false
+    }
+  }
+  
+  return true
+}
