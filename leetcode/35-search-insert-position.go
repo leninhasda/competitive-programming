33cfeb8c88ebd94e44bd192a49a631d80d6446f1
Lenin Hasda <leninhasda@gmail.com>
2019-01-15 10:12:58 +0600

    leetcode search insert position

---
+++ leetcode/35-search-insert-position.go
@@ -0,0 +1,18 @@
+func searchInsert(nums []int, target int) int {
+  return bSearch(nums, target, 0, len(nums)-1)
+}
+
+func bSearch(ns []int, val, left, right int) int {
+  if right < left {
+    return left
+  }
+  mid := (left+right)/2
+  if ns[mid] == val {
+    return mid
+  }
+  if val < ns[mid] {
+    return bSearch(ns, val, left, mid-1)
+  } else {
+    return bSearch(ns, val, mid+1, right)
+  }
+}
