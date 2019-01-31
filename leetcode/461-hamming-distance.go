e28123e6acf832079d09327c666b8b709d958d53
Lenin Hasda <lenin.hasda@codersrank.io>
2019-01-31 23:55:33 +0600

    hamming distance delete columns to make sorted

---
+++ leetcode/461-hamming-distance.go
@@ -0,0 +1,24 @@
+import "strconv"
+func hammingDistance(x int, y int) int {
+	xs := strconv.FormatInt(int64(x), 2)
+	ys := strconv.FormatInt(int64(y), 2)
+
+	if len(xs) > len(ys) {
+		xs, ys = ys, xs
+	}
+
+	ldif := len(ys) - len(xs)
+	bt := make([]byte, ldif)
+	for i := 0; i < ldif; i++ {
+		bt[i] = '0'
+	}
+	xs = string(bt) + xs
+	var cnt int
+	println(xs, ys)
+	for i := range xs {
+		if xs[i] != ys[i] {
+			cnt++
+		}
+	}
+	return cnt
+}
---
+++ leetcode/944-delete-columns-to-make-sorted.go
@@ -0,0 +1,12 @@
+func minDeletionSize(A []string) int {
+	var d int
+	for c, _ := range A[0] {
+		for i := 1; i < len(A); i++ {
+			if A[i][c] < A[i-1][c] {
+				d++
+				break
+			}
+		}
+	}
+	return d
+}
