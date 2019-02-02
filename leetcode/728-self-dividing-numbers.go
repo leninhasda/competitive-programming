9c569c95e988cdde757a7de94d0b513bb5d044f1
Lenin Hasda <lenin.hasda@codersrank.io>
2019-02-02 23:28:49 +0600

    self dividing numbers

---
+++ leetcode/728-self-dividing-numbers.go
@@ -0,0 +1,29 @@
+var mp = make(map[int]struct{})
+func selfDividingNumbers(left int, right int) []int {
+  ret := []int{}
+  for i:=left; i<=right; i++ {
+    if _, ok := mp[i]; ok {
+      ret = append(ret, i)
+    } else if isSelfDiv(i){
+      mp[i] = struct{}{}
+      ret = append(ret, i)
+    }
+  } 
+  return ret 
+}
+
+func isSelfDiv(i int) bool {
+  var m int
+  tmpI := i
+  for true {
+    m, i  = i % 10, i / 10 
+    
+    if m==0 || (tmpI % m) != 0 {
+      return false 
+    }
+    if i == 0 {
+      break
+    }
+  } 
+  return true
+}
