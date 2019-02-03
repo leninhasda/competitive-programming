20f4eb4f861c534091a2647e2885327973921a54
Lenin Hasda <lenin.hasda@codersrank.io>
2019-02-03 21:57:37 +0600

    sum of even numbers after queries

---
+++ leetcode/985-sum-of-even-numbers-after-queries.go
@@ -0,0 +1,22 @@
+func sumEvenAfterQueries(A []int, queries [][]int) []int {
+  ret := make([]int, len(queries))   
+  av := 0
+  val, I := 0, 0
+  for idx, q := range queries{
+    av = 0
+    val, I = q[0], q[1] 
+    for i, v := range A {
+      if i == I {
+        v += val
+        A[i] = v
+      }
+      if v % 2 == 0 {
+        av += v
+      }
+    } 
+    ret[idx] = av 
+  }
+  return ret
+}
+
+
