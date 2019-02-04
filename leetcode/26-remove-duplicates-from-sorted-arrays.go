0325bf6bd8908c13e3bfe125b7d6c6996c2a367f
Lenin Hasda <lenin.hasda@codersrank.io>
2019-02-04 23:23:53 +0600

    remove duplicates from sorted arrays

---
+++ leetcode/26-remove-duplicates-from-sorted-arrays.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+  if len(nums) == 0 {
+    return 0
+  }
+
+  rid:=0
+  for i:=1; i<len(nums); i++ {
+    if nums[rid] != nums[i] {
+      rid++
+      nums[rid] = nums[i] 
+    }   
+  }
+
+  return rid + 1
+}
+
