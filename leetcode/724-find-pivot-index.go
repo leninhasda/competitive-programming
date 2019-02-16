c923ca6b17c8b9257af0e22ef4a5590faf7bba95
Lenin Hasda <leninhasda@gmail.com>
2019-02-16 23:08:03 +0600

    find pivot index

---
+++ leetcode/724-find-pivot-index.go
@@ -0,0 +1,21 @@
+func pivotIndex(nums []int) int {
+  total := 0
+  sums := make([]int, len(nums))
+  
+  for i, n := range nums {
+    total += n
+    sums[i] = total
+  }
+  
+  for i, n := range nums {
+    prev := 0 
+    if i-1 >=0 {
+      prev = sums[i-1]
+    }
+    if (total-n) == (prev*2) {
+      return i
+    }
+  }
+  
+  return -1
+}
